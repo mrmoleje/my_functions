c0cccb99f7750614fb358d090fa900e75d546eb2
Sue Wallace <34483289+mrmoleje@users.noreply.github.com>
2019-06-06 16:06:08 +0100

    Add files via upload

---
+++ functions.r
@@ -0,0 +1,60 @@
+# Sue Wallace
+# 06.06.2019
+
+# One place for useful functions
+
+# data manipulation----
+
+# Arranging variables
+
+arrange.vars <- function(data, vars){
+  ##stop if not a data.frame (but should work for matrices as well)
+  stopifnot(is.data.frame(data))
+  
+  ##sort out inputs
+  data.nms <- names(data)
+  var.nr <- length(data.nms)
+  var.nms <- names(vars)
+  var.pos <- vars
+  ##sanity checks
+  stopifnot( !any(duplicated(var.nms)), 
+             !any(duplicated(var.pos)) )
+  stopifnot( is.character(var.nms), 
+             is.numeric(var.pos) )
+  stopifnot( all(var.nms %in% data.nms) )
+  stopifnot( all(var.pos > 0), 
+             all(var.pos <= var.nr) )
+  
+  ##prepare output
+  out.vec <- character(var.nr)
+  out.vec[var.pos] <- var.nms
+  out.vec[-var.pos] <- data.nms[ !(data.nms %in% var.nms) ]
+  stopifnot( length(out.vec)==var.nr )
+  
+  ##re-arrange vars by position
+  data <- data[ , out.vec]
+  return(data)
+}
+
+# Rather than having to specify every variable you can rearrange just a few and 
+# only call those that you are rearranging
+
+arrange.vars(my_data, c("time"=1, "date" = 2, "geog" = 3)) -> rearranged_data
+
+
+
+
+# Rounding
+
+# Round up - like excel. R rounds to the even number which isn;t always compatible with
+# other software when QAing. 
+
+round_up = function(x, n) {
+  posneg = sign(x)
+  z = abs(x)*10^n
+  z = z + 0.5
+  z = trunc(z)
+  z = z/10^n
+  z*posneg
+}
+
